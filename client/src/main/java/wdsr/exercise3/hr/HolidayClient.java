bfbba74ba04a3f3c92071c20d858dbe23443d823
DonLeo93 <raf.szczepaniak@gmail.com>
2016-04-29 10:33:52 +0200

    Done

--- client/src/main/java/wdsr/exercise3/hr/HolidayClient.java
+++ client/src/main/java/wdsr/exercise3/hr/HolidayClient.java
@@ -2,6 +2,11 @@ package wdsr.exercise3.hr;
 
 import java.net.URL;
 import java.util.Date;
+import java.util.GregorianCalendar;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.ws.soap.SOAPFaultException;
 
 // TODO Complete this class to book holidays by issuing a request to Human Resource web service.
 // In order to see definition of the Human Resource web service:
@@ -12,8 +17,13 @@ public class HolidayClient {
 	 * Creates this object
 	 * @param wsdlLocation URL of the Human Resource web service WSDL
 	 */
+	
+	private HumanResourceService service; 
+	private HumanResource hr;
+	
 	public HolidayClient(URL wsdlLocation) {
-		// TODO
+		service = new HumanResourceService(wsdlLocation);
+		hr = service.getHumanResourcePort();
 	}
 	
 	/**
@@ -27,7 +37,27 @@ public class HolidayClient {
 	 * @throws ProcessingException if request processing fails.
 	 */
 	public int bookHoliday(int employeeId, String firstName, String lastName, Date startDate, Date endDate) throws ProcessingException {
-		// TODO
-		return 0;
+		EmployeeType employee = new EmployeeType();
+		HolidayType holiday = new HolidayType();
+		employee.setNumber(employeeId);
+		employee.setFirstName(firstName);
+		employee.setLastName(lastName);
+		try {
+			GregorianCalendar c = new GregorianCalendar();
+			c.setTime(startDate);
+			holiday.setStartDate(DatatypeFactory.newInstance().newXMLGregorianCalendar(c));
+			c.setTime(endDate);
+			holiday.setEndDate(DatatypeFactory.newInstance().newXMLGregorianCalendar(c));
+		} catch (DatatypeConfigurationException e) {
+			e.printStackTrace();
+		}
+		HolidayRequest request = new HolidayRequest();
+		request.setEmployee(employee);
+		request.setHoliday(holiday);
+		try{
+			return hr.holiday(request).getRequestId();
+		}catch(SOAPFaultException e){
+			throw new ProcessingException();
+		}
 	}
 }
